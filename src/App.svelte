3b3de556478691f556e79bc528f1a3e515e89890
nomzy-kush <nomzykush@gmail.com>
2021-10-15 02:44:14 -0700

    some-changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
+
+
+
 <script>
-	import { Button } from 'svelte-mui/src'
+	import { Button } from 'svelte-mui/src';
+	import Header from "./components/Header.svelte";
+	import {MDCTextField} from '@material/textfield';
+
+	
 	let tasks = [];
 	const addTasks = () => {
 		tasks = [...tasks, '']
@@ -12,37 +19,90 @@
 
 </script>
 
-<main>
-	<article>
-
-{#each tasks as task, index }
- <input bind:value={tasks[index]}>
- <Button on:click={()=>removeTask(index)} raised color="#737383">Remove</Button>
- <br>
- {/each}
-
-
-
- <Button on:click={addTasks} raised color="#ff4d14">Add
-	</Button>
-
-</article>
-</main>
-
-
 <style>
 
 	main {
 		display: flex;
 		justify-content: center;
 	}
+ 
 
-	input {
-		border-radius: 3px;
-		width: 10em;
-		height: 3em;
-		color: #FFC0CB;
-		background-color: #000;
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+		padding: 3em 3em
 	}
 
+	.add-btn {
+		display: flex;
+		justify-content: center;
+		padding-bottom: 1em;
+	}
+
+	.container {
+		width: 500px;
+		background: #C0C0C0;
+	}
+
+	:global( .container *) {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+	}
+
+
+
+
+
+
 </style>
+
+
+
+
+<main>
+	<head>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	  </head>
+	<div class="container">
+		<Header />
+
+		<div class="grid">
+
+{#each tasks as task, index }
+<div class="grid-1">
+	<label class="mdc-text-field mdc-text-field--outlined">
+		<span class="mdc-notched-outline">
+		  <span class="mdc-notched-outline__leading"></span>
+		  <span class="mdc-notched-outline__notch">
+			<span class="mdc-floating-label" id="my-label-id"></span>
+		  </span>
+		  <span class="mdc-notched-outline__trailing"></span>
+		</span>
+		<input type="text" class="mdc-text-field__input" aria-labelledby="my-label-id">
+	  </label>
+</div>
+
+<div class="grid-2">
+ 
+ <Button on:click={()=>removeTask(index)} > <span class="material-icons" >
+	delete
+	</span> </Button>
+</div>
+ {/each}
+
+</div>
+
+
+<div class="add-btn">
+ <Button on:click={addTasks} raised color="#ff4d14" >Add
+	</Button>
+</div>
+
+	
+
+	
+
+</div>
+</main>
+
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    
+</script>
+
+<style>
+    .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 20px;
+        color: #fff;
+        font-weight: bold;
+        font-size: 1.5em;
+        background: rgba(0, 0, 0, 0.1);
+    }
+
+</style>
+
+
+
+<div class="header">
+    <span>Onome's Todo list</span>
+
+</div>
